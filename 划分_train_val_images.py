302898bdf859319c64e1728d621fcdfb53d70414
XiaoTaowen <118878213+TW-Xiao@users.noreply.github.com>
2023-05-07 13:03:06 +0800

    Update 划分_train_val_images.py

--- 划分_train_val_images.py
+++ 划分_train_val_images.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 from PIL import Image
 import numpy as np
 
-folder_dir = '/home/icisee/XTW/声学/pytorch_simple_CenterNet_45-master/data/coco'
+folder_dir = ''
 txt_dir = 'val2017'
 
 #### 挑出1000张原图片
@@ -13,9 +13,9 @@ data_folder = os.path.join(folder_dir, txt_dir)  # 文件路径
 for v in os.listdir(data_folder):
     v = os.path.splitext(v)[0]
     # 图片路径，相对路径
-    image_path = r'/home/icisee/XTW/声学/pytorch_simple_CenterNet_45-master/data/VOCdevkit/VOC2007/JPEGImages_break'
+    image_path = r''
     image_name = v
-    image_path1 = r'/home/icisee/XTW/声学/pytorch_simple_CenterNet_45-master/data/VOCdevkit/VOC2007/val2017_break'
+    image_path1 = r''
     print(v)
     image_path_name = os.path.join(image_path, image_name + ".bmp")
     image_path_name1 = os.path.join(image_path1, image_name + ".bmp")
